dd0d58cb36a822d450a8c1ee1348efc1af0dc002
kyotenkacat <kyotenkacat@gmail.com>
2020-03-07 01:10:23 +0800

    add doc

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, '../docs/index.html'),
     publicPath: '/dist/',
     filename: 'build.js'
   },
